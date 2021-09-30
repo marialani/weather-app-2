68cc3e8b76853712175b990f352b9d844fe2bbf9
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-30 19:20:26 +0100

    added file structure

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import "../styles/index.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="prop">
+      <h1>Weather App</h1>
+    </div>
+  );
+}
+
+export default App;
